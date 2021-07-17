c0297eb1052d0fe0f70af891f3f6cc246a9fd077
dev-talbi <dev.talbi@gmail.com>
2021-07-17 19:54:00 +0200

    in progress

--- src/App.js
+++ src/App.js
@@ -4,7 +4,23 @@ import Contact from './Components/Contact';
 function App() {
   return (
     <div className="App">
-      <Contact/>
+      <Contact
+      name="plopinette"
+      avatar="https://randomuser.me/api/portraits/women/69.jpg"
+      statusOnline
+      online
+      />
+
+      <Contact
+      name="Gloria"
+      avatar="https://randomuser.me/api/portraits/women/79.jpg"
+      />
+
+      <Contact
+      name="Richard"
+      avatar="https://randomuser.me/api/portraits/men/57.jpg"
+      online
+      />
     </div>    
   );
 }
--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './Contact.css';
 
 
-function Contact() {
+function Contact(props) {
     return (
         <div className="Contact">
-            <img className="avatar" src="https://randomuser.me/api/portraits/men/49.jpg" alt="avatar" />
+            <img className="avatar" src={props.avatar} alt="avatar" />
             <div className="status">
-            <p className="name">Jared</p>
-                <div className="status-online"></div>
-                <p className="status-text">Online</p>
+            <p className="name">{props.name}</p>
+                <div className={props.statusOnline ?"status-online" :"status-offline" }></div>
+                <p className="status-text">{props.online ? 'Online' : 'Offline'}</p>
             </div>
         </div>
     )
